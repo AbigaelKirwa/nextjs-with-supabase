232f62701d04e961ff04fc8d5c4bdf5165ae358e
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-28 22:54:11 +0300

    feat(delete):enabled deleting of a row in the table

---
+++ app/todo/actions.tsx
@@ -0,0 +1,18 @@
+'use server'
+import { createClient } from "@/utils/supabase/server"
+const supabase = createClient()
+
+//delete
+export async function deleteAction({id}:{id:any}) {
+    console.log("Deleted successfully")
+    const { error } = await supabase.from('todo').delete().eq('id', id)
+    if(error){throw new Error("Did not fetch component")}
+}
+
+// read
+export async function todoLoader() {
+    const { data: toDoItems, error } = await supabase.from('todo').select('*');
+    if(error){throw new Error("Did not fetch component")}
+    return toDoItems;
+}
+
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,10 +1,11 @@
-'use server'
-import { createClient } from "@/utils/supabase/server"
+'use client'
+import { useState, useEffect} from "react"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Badge } from "@/components/ui/badge"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
-
+import { todoLoader, deleteAction } from '@/app/todo/actions'
+import { useRouter } from "next/navigation";
 
 const tableHeaderNames=[
     "Task Number",
@@ -14,22 +15,46 @@ const tableHeaderNames=[
     "Actions"
 ]
 
-export default async function ToDO(){
-    const supabase = createClient()
-    let { data: toDoItems } = await supabase.from('todo').select('*')
-    // const todo_array = JSON.stringify(todo)
+export default function ToDO(){
+    //fetching data 
+    const [toDoItems, setToDoItems] = useState<any>([]);
+    const router = useRouter()
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const loadedToDoItems = await todoLoader();
+                setToDoItems(loadedToDoItems);
+            } catch (error) {
+                console.error('Failed to load todo items:', error);
+            }
+        }
+        fetchData();
+    }, [toDoItems]);
+
+    //delete
+    const handleDelete = async (id:any) => {
+        try {
+            await deleteAction({id});
+            router.refresh()
+        } catch (error) {
+            console.error('Error deleting todo item:', error);
+        }
+    };
+
     if (toDoItems !== null){
         return (
             <div className="mt-10">
                 <h1 className="font-bold text-5xl text-center">To Do Application</h1>      
                 <Table className="mt-10">
                     <TableHeader>
+                    <TableRow>
                     {tableHeaderNames.map((tableHeaderName)=>(
-                        <TableHead className="text-center font-semibold text-base text-slate-700">{tableHeaderName}</TableHead>
+                        <TableHead key={tableHeaderName} className="text-center font-semibold text-base text-slate-700">{tableHeaderName}</TableHead>
                     ))}
+                    </TableRow>
                     </TableHeader>
                     <TableBody>
-                    {toDoItems.map((todoItem)=>(
+                    {toDoItems && toDoItems.length > 0 ? toDoItems.map((todoItem:any)=>(
                         <TableRow key={todoItem.id}>
                             <TableCell>{todoItem.id}</TableCell>
                             <TableCell className="text-center">{todoItem.name}</TableCell>
@@ -45,13 +70,18 @@ export default async function ToDO(){
                             </TableCell>
                             <TableCell>
                                 <Button variant="secondary" className=" bg-blue-700 text-white">Edit</Button>
-                                <Button variant="destructive" className="ml-5 bg-[#C30010] text-white">Delete</Button>
+                                <Button variant="destructive" className="ml-5 bg-[#C30010] text-white" onClick={()=>handleDelete(todoItem.id)}>Delete</Button>
                             </TableCell>
                         </TableRow>
-                    ))}
+                    )) : (
+                        <TableRow>
+                            <TableCell colSpan={5}>No tasks available</TableCell>
+                        </TableRow>
+                    )}
                     </TableBody>
                 </Table>
             </div>  
         )
     }
 }
+
