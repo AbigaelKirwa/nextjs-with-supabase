542a39e0d1734451e2b77c73f6fdffb62a1669b1
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-29 18:07:29 +0300

    feat(create):creates todo task through a form

--- app/todo/actions.tsx
+++ app/todo/actions.tsx
@@ -1,18 +1,25 @@
 'use server'
 import { createClient } from "@/utils/supabase/server"
 const supabase = createClient()
+import { toDoItemsType } from "@/types"
 
 //delete
-export async function deleteAction({id}:{id:any}) {
+export async function deleteAction({id}:{id:number}) {
     console.log("Deleted successfully")
     const { error } = await supabase.from('todo').delete().eq('id', id)
     if(error){throw new Error("Did not fetch component")}
 }
 
 // read
-export async function todoLoader() {
+export async function readAction() {
     const { data: toDoItems, error } = await supabase.from('todo').select('*');
-    if(error){throw new Error("Did not fetch component")}
+    if(error){throw new Error("Did not fetch data")}
     return toDoItems;
+}
+
+//create
+export async function  createAction({name, priority, done}:toDoItemsType){
+    const { error } = await supabase.from('todo').insert({ name: name, priority:priority, done:done })
+    if(error){throw new Error("Did not create row")}
 }
 
