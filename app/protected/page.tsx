e393e48f497a93f48a8c45af94b77fd47978a7a0
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-05-01 11:19:29 +0300

    refractor:places todo files in protected folder so that user has to login to view todo tasks

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,8 +1,8 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import ToDO from "../todo/page";
+import ToDO from "./components/todo";
+import ToDoHeader from "./components/todoHeader";
+import ToDoFooter from "./components/todoFooter";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -17,35 +17,9 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-3 items-center" style={{opacity:1}}>
-      <div className="w-full">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
-            <AuthButton />
-          </div>
-        </nav>
-      </div>
-
+      <ToDoHeader/>
       <ToDO/>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6">
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
+      <ToDoFooter/>
     </div>
   );
 }
